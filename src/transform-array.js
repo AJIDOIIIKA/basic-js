60978055d614ada1209d1a45f85f1df853a69b08
Aldiyar <aaaldiyar.98@gmail.com>
2020-11-01 20:02:55 +0600

    feat: add task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  // throw new CustomError('Not implemented');
+  let array = arr.map(function(item, index,arr){
+      if(item == '--double-next'){
+      arr[index] = arr[index + 1]; 
+      }
+      else if (item == '--double-prev') {
+          arr[index] = arr[index - 1]; 
+      }
+     else if (item == '--discard-next') {
+          [arr[index + 1], arr[index]] = [undefined, undefined]; 
+      }
+     else if (item == '--discard-prev') {
+          [arr[index + 1], arr[index]] = [undefined, undefined];
+      }
+    return arr;
+      });
+   ([...arr]).filter((element) => element !== undefined)
+  
+  
 };
